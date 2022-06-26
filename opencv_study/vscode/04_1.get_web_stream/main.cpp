0a339807c90d405f6d85ad5b20658eb098358a74
Park jae wan <hanulcom7@naver.com>
2022-06-26 11:30:36 +0900

    test webcam
    test webcam

--- opencv_study/vscode/04_1.get_web_stream/main.cpp
+++ opencv_study/vscode/04_1.get_web_stream/main.cpp
@@ -9,7 +9,8 @@
   ///////////////// Webcam ////////////////////// not test yet!
  
 int main() {
-    VideoCapture cap("http://cloud.park-cloud.co19.kr:8002/");
+    // VideoCapture cap("http://cloud.park-cloud.co19.kr:8002/");
+    VideoCapture cap(1);//0:mac webcam 1: usb webcam
     Mat img;
     while (true) {
     cap.read(img);
