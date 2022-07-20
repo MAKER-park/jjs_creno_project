386f10697ec54e063f0287f58cdccec1f266404f
Kang Hyeon Gu <grains01@naver.com>
2022-07-20 11:39:01 +0900

    still not finish

---
+++ Dynamixel/Dynamixel_Floor_calc/Dynamixel_Floor_calc.ino
@@ -0,0 +1,54 @@
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+#include <Dynamixel2Arduino.h>
+#include <Servo.h>
+
+#include <ros.h>
+#include <std_msgs/UInt16.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Empty.h>
+
+#define DXL_SERIAL   Serial3
+#define DEBUG_SERIAL Serial
+
+#define home 0
+#define firstFloor 3000
+#define secondFloor 2500
+
+const int DXL_DIR_PIN = 84;
+const uint8_t DXL_ID = 1;
+const float DXL_PROTOCOL_VERSION = 2.0;
+
+ros::NodeHandle  nh;
+
+Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
+
+void setup() {
+  // put your setup code here, to run once:
+  // Use Serial to debug.
+  DEBUG_SERIAL.begin(57600);
+  // Set Port baudrate to 57600bps. This has to match with DYNAMIXEL baudrate.
+  dxl.begin(57600);
+  // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
+  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+  // Get DYNAMIXEL information
+  dxl.ping(DXL_ID);
+
+  dxl.torqueOff(DXL_ID);
+  dxl.setOperatingMode(DXL_ID, OP_EXTENDED_POSITION);
+  dxl.torqueOn(DXL_ID);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  dxl.setGoalPosition(DXL_ID, 9000);
+  delay(5000);
+  dxl.setGoalPosition(DXL_ID, 5000);
+  delay(5000);
+  dxl.setGoalPosition(DXL_ID, 0);
+  delay(5000);
+
+}
---
+++ Dynamixel/Dynamixel_Ros/Dynamixel_Ros.ino
@@ -0,0 +1,101 @@
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+#include <Dynamixel2Arduino.h>
+#include <Servo.h>
+
+#include <ros.h>
+#include <std_msgs/UInt16.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Empty.h>
+
+#define DXL_SERIAL   Serial3
+#define DEBUG_SERIAL Serial
+
+#define home 0
+#define firstFloor 9000
+#define secondFloor 7500
+
+const int DXL_DIR_PIN = 84;
+const uint8_t DXL_ID = 1;
+const float DXL_PROTOCOL_VERSION = 2.0;
+
+ros::NodeHandle  nh;
+
+Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
+Servo servo1;
+Servo servo2;
+
+//This namespace is required to use Control table item names
+using namespace ControlTableItem;
+
+void hold(int value);
+void putDown(int value);
+
+void move_dn(const std_msgs::String &cmd_msg)
+{
+  String temp = cmd_msg.data;
+  int result[2] = { -1, -1 };
+  String search[2] = {"hold", "putDown"};
+  for (int i = 0; i < 2; i++) {
+    result[i] = temp.compareTo(search[i]);
+  }
+  if (result[0] == 0)
+    hold(firstFloor);
+  else if (result[1] == 0)
+    putDown(firstFloor);
+}
+
+//ros를 사용하기 위한 명령어
+ros::Subscriber<std_msgs::String> sub1("grep_move", move_dn);
+//ros::Subscriber<std_msgs::Empty> sub2();
+
+void setup() {
+  // put your setup code here, to run once:
+  servo1.attach(9);
+  servo2.attach(10);
+  // Use Serial to debug.
+  DEBUG_SERIAL.begin(57600);
+  // Set Port baudrate to 57600bps. This has to match with DYNAMIXEL baudrate.
+  dxl.begin(57600);
+  // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
+  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+  // Get DYNAMIXEL information
+  dxl.ping(DXL_ID);
+  
+  dxl.torqueOff(DXL_ID);
+  dxl.setOperatingMode(DXL_ID, OP_EXTENDED_POSITION);
+  dxl.torqueOn(DXL_ID);
+}
+
+void loop() { //루프문은 거드리지 말것 나머지는 함수로 구현
+  // put your main code here, to run repeatedly:
+  nh.spinOnce();
+  delay(1);
+}
+
+void hold(int value)//집게를 내려서 컨테이너를 잡고 올라옴
+{
+  servo1.write(150);
+  servo2.write(150);
+  dxl.setGoalPosition(DXL_ID, value);
+  delay(1500);
+  servo1.write(50);
+  servo2.write(50);
+  delay(1000);
+  dxl.setGoalPosition(DXL_ID, home);
+}
+
+void putDown(int value)//집게를 내려서 컨테이너를 내리고 올라옴
+{
+  servo1.write(50);
+  servo2.write(50);
+  dxl.setGoalPosition(DXL_ID, value);
+  delay(1500);
+  servo1.write(150);
+  servo2.write(150);
+  delay(1000);
+  dxl.setGoalPosition(DXL_ID, home);
+}
